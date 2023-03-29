cdb8775b68d7c3daa07a4e6c424dcf74f1c8bac1
lucasmessias3 <lucasmessias@live.com>
2023-03-29 10:42:15 -0300

    Added Dismissible option in the Cart

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,8 @@
 import 'package:app_04/models/cart_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
   const CartItemWidget({super.key, required this.cartItem});
@@ -7,32 +10,56 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Colors.redAccent,
-          child: Padding(
-            padding: const EdgeInsets.all(2),
-            child: FittedBox(
-              child: Text(
-                cartItem.price.toStringAsFixed(2),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.redAccent,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.all(15),
+        child: const Icon(
+          color: Colors.white,
+          Icons.delete,
+          size: 40,
+        ),
+      ),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.redAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(2),
+              child: FittedBox(
+                child: Text(
+                  cartItem.price.toStringAsFixed(2),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        title: Text(
-          cartItem.name,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
+          title: Text(
+            cartItem.name,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
         ),
-        subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
-        trailing: Text('${cartItem.quantity}x'),
       ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
     );
   }
 }
